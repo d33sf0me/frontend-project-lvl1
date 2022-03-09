5b755c942f7ffa7882012cc66cdd47bd92ba7c5c
d33sf0me <katekrokh@gmail.com>
2022-03-09 22:24:05 +0300

    add correct version of calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -1,12 +1,13 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-eval */
 import readlineSync from 'readline-sync';
 import { name } from './cli.js';
 // eslint-disable-next-line linebreak-style
 
-/* function whatSign(sign) {
-    let signs=['+','-','*'];
-    let i=Math.floor(Math.random()*sign.length);
-    return signs[i];
+export default function whatSign() {
+  const signs = ['+', '-', '*'];
+  const i = Math.floor(Math.random() * signs.length);
+  return signs[i];
 }
 
 console.log('What is the result of the expression?');
@@ -14,21 +15,13 @@ let counter = 0;
 while (counter < 3) {
   const number1 = Math.floor(Math.random() * 101); // returns a random integer from 0 to 100
   const number2 = Math.floor(Math.random() * 101); // returns a random integer from 0 to 100
-
-  console.log(`Question: ${number1} + ${number2}`);
+  const sign = whatSign();
+  const expression = number1 + sign + number2;
+  console.log(`Question: ${expression} `);
   const answer = readlineSync.question('Your answer: ');
-  if (IsEven(number) === true && answer === 'yes') {
+  // eslint-disable-next-line eqeqeq
+  if (answer == eval(expression)) {
     console.log('Correct!');
     counter += 1;
-  } else if (IsEven(number) === undefined && answer === 'no') {
-    console.log('Correct!');
-    counter += 1;
-  } else if (IsEven(number) === undefined && answer === 'yes') {
-    console.log(`"yes" is wrong answer ;(. Correct answer was "no". \nLet's try again, ${name}!`);
-    break;
-  } else if (IsEven(number) === true && answer === 'no') {
-    console.log(`"no" is wrong answer ;(. Correct answer was "yes". \nLet's try again, ${name}!`);
-    break;
-  }
+  } else { console.log(`'${answer}' is wrong answer ;(. Correct answer was '${eval(expression)}'.\nLet's try again, ${name}!`); break; }
 } if (counter === 3) { console.log(`Congratulations, ${name}!`); }
-*/
